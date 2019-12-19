61ba9d94bada2238a5a64112ce49b5706be71621
katriel <katriel181920@gmail.com>
2019-12-18 22:58:31 -0500

    ALFA 2

--- servidor/config/config.js
+++ servidor/config/config.js
@@ -16,15 +16,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'DESARROLLO';
 //BASE DE DATOS
 //==================================
 let urlDB;
-/*
+
 if (process.env.NODE_ENV === 'DESARROLLO') {
     urlDB = 'mongodb://localhost:27017/BD_PAGINA_WEB_01';
 
 } else {
 
-*/
-urlDB = process.env.MONGO_URI;
-/* // urlDB = "mongodb+srv://katriel:katriel@cluster0-5fhsz.mongodb.net/BD_PAGINA_WEB_01?retryWrites=true&w=majority";
 
-}*/
+    urlDB = process.env.MONGO_URI; //mongodb+srv://katriel:katriel@cluster0-5fhsz.mongodb.net/BD_PAGINA_WEB_01?retryWrites=true
+    // urlDB = "mongodb+srv://katriel:katriel@cluster0-5fhsz.mongodb.net/BD_PAGINA_WEB_01?retryWrites=true&w=majority";
+
+}
 process.env.URLDB = urlDB;
