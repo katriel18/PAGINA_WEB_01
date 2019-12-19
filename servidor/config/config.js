41a0b10511f58ce6f024b2be86150e508265f4e3
katriel <katriel181920@gmail.com>
2019-12-18 22:47:54 -0500

    ALFA ACABADO ONLINE BD

--- servidor/config/config.js
+++ servidor/config/config.js
@@ -10,21 +10,21 @@ process.env.PORT = process.env.PORT || 3000;
 //===================================
 //terminal
 //==================================
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'DESARROLLO';
 
 //===================================
 //BASE DE DATOS
 //==================================
 let urlDB;
-
-if (process.env.NODE_ENV === 'dev') {
+/*
+if (process.env.NODE_ENV === 'DESARROLLO') {
     urlDB = 'mongodb://localhost:27017/BD_PAGINA_WEB_01';
 
 } else {
 
+*/
+urlDB = process.env.MONGO_URI;
+/* // urlDB = "mongodb+srv://katriel:katriel@cluster0-5fhsz.mongodb.net/BD_PAGINA_WEB_01?retryWrites=true&w=majority";
 
-    //urlDB = process.env.MONGO_URI;
-    urlDB = "mongodb+srv://katriel:katriel@cluster0-5fhsz.mongodb.net/BD_PAGINA_WEB_01?retryWrites=true&w=majority";
-
-}
+}*/
 process.env.URLDB = urlDB;
